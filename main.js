fc2dd78789e29daf056d4cf3292258eadc0ac55e
BidyanshIndian <71256652+BidyanshIndian@users.noreply.github.com>
2020-12-15 12:58:33 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
-function addUser(){
-    user_name= document.getElementById("user_name").value;
-    localStorage.setItem("user_name",user_name);
-    window.location= "kwitter_room.html";
+var firebaseConfig = {
+    apiKey: "AIzaSyDOn2wB_1FNhfZAikCUIxyzfFG41NkXZF4",
+    authDomain: "let-s-chat-up.firebaseapp.com",
+    databaseURL: "https://let-s-chat-up-default-rtdb.firebaseio.com",
+    projectId: "let-s-chat-up",
+    storageBucket: "let-s-chat-up.appspot.com",
+    messagingSenderId: "988861679539",
+    appId: "1:988861679539:web:a159a1efca3feacbf72dc6"
+  };
+  
+  firebase.initializeApp(firebaseConfig);
+
+function addUser()
+{
+  user_name = document.getElementById("user_name").value;
+  firebase.database().ref("/").child(user_name).update({
+    purpose : "adding user"
+  });
 }
